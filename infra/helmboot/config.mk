59837eed3b26e38a7ffba008e936907e959b401e
Stephane Lacoin (aka nxmatic) <slacoin@nuxeo.com>
2020-05-09 17:45:53 +0200

    fixup stack config

--- infra/helmboot/config.mk
+++ infra/helmboot/config.mk
@@ -1,5 +1,5 @@
-stack-config: github-config
-stack-config: boot-secrets-config
+pulumi-stack-config: github-config
+pulumi-stack-config: boot-secrets-config
 
 github-config:
 	@:$(call check-variable-defined,git-owner dev-repository)
