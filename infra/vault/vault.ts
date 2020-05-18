fe6deb3abbf2b94518435bba4ccb17b34d1b1fe5
Stephane Lacoin (aka nxmatic) <slacoin@nuxeo.com>
2020-05-18 12:03:17 +0200

    fixed vault infra setup

--- infra/vault/vault.ts
+++ infra/vault/vault.ts
@@ -1,22 +1,30 @@
 import * as gcp from "@pulumi/gcp";
+import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
-import { clusterName, keyringName, rfc1035 } from "./config";
+import * as controlPlane from "../control-plane/output";
+import * as helmboot from "../helmboot/output";
+import * as _ from "./config";
+
+const k8sProvider = controlPlane.output.k8sProvider();
+const clusterName = controlPlane.output.clusterName;
+const accountId = clusterName.apply(v => _.rfc1035(v).id()).apply(v => `${v}-vt`);
+const appsNamespace = helmboot.output.appsNamespace;
 
 export const bucket = new gcp.storage.Bucket("vault", {
     location: gcp.config.region,
-    name: `${clusterName}-vault`,
+    name: pulumi.interpolate`${clusterName}-vault`,
 });
 export const cryptoKey = new gcp.kms.CryptoKey("vault", {
-    keyRing: `projects/build-jx-prod/locations/${gcp.config.region}/keyRings/${clusterName}`,
+    keyRing: pulumi.interpolate`projects/build-jx-prod/locations/${gcp.config.region}/keyRings/${clusterName}`,
     rotationPeriod: "100000s",
 });
 export const serviceAccount = new gcp.serviceAccount.Account("vault", {
-    accountId: rfc1035(`${clusterName}-vt`).id(),
-    displayName: `Vault service account for ${clusterName}`,
+    accountId: accountId,
+    displayName: pulumi.interpolate`vault service account for ${clusterName}`,
 });
 export const serviceAccountKey = new gcp.serviceAccount.Key("vault", {
     publicKeyType: "TYPE_X509_PEM_FILE",
-    serviceAccountId: serviceAccount.name,
+    serviceAccountId: pulumi.interpolate`${serviceAccount.name}`,
 });
 export const storageObjectAdminBinding = new gcp.projects.IAMMember("vault-storage-object-admin-binding", {
     member: pulumi.interpolate`serviceAccount:${serviceAccount.email}`,
@@ -34,7 +42,7 @@ export const secret = new k8s.core.v1.Secret("vault-sa",
     {
         metadata: {
             name: "vault-sa",
-            namespace: systemNamespace,
+            namespace: appsNamespace,
             labels: { app: "helmboot" }
         },
         type: "Opaque",
