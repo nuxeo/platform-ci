d5459ab003999de8667444ba95b286af648e2d82
Antoine Taillefer <ataillefer@nuxeo.com>
2020-07-23 12:00:19 +0200

    NXBT-3352: Adapt namespaces for Platform

--- infra/namespaces/namespaces.ts
+++ infra/namespaces/namespaces.ts
@@ -7,21 +7,23 @@ import * as controlPlane from "../control-plane/output";
 const k8sProvider = controlPlane.output.k8sProvider();
 const clusterName = controlPlane.output.clusterName;
 
-export const appsNamespace = new k8s.core.v1.Namespace("jx",
+export const appsNamespace = new k8s.core.v1.Namespace("jenkins",
     {
         metadata: {
-            name: "jx"
+            name: "jenkins"
         }
     },
-    { provider: k8sProvider });
+   { provider: k8sProvider });
+// if the namespace already exists
 //  { provider: k8sProvider, import: "jx" });
 
-export const systemNamespace = new k8s.core.v1.Namespace("jx-system",
+export const systemNamespace = new k8s.core.v1.Namespace("jenkins-system",
     {
         metadata: {
-            name: "jx-system"
+            name: "jenkins-system"
         }
     },
     { provider: k8sProvider });
+// if the namespace already exists
 //  { provider: k8sProvider, import: "jx-system" });
 
