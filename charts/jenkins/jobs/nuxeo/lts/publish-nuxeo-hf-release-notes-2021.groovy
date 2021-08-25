583f5235c6de01beeb5214b5ce3eb0a2aa2a50ec
Antoine Taillefer <ataillefer@nuxeo.com>
2021-08-25 17:42:11 +0200

    NXBT-3498: Add job to update HF description for LTS 2021

---
+++ charts/jenkins/jobs/nuxeo/lts/publish-nuxeo-hf-release-notes-2021.groovy
@@ -0,0 +1,42 @@
+pipelineJob('nuxeo/lts/publish-nuxeo-hf-release-notes-2021') {
+  // Description
+  description('Update Hotfix package description with the JIRA release notes.')
+  definition {
+    // Definition
+    cpsScm {
+      scm {
+        // SCM
+        git {
+          // Repositories
+          remote {
+            // Repository URL
+            url('https://github.com/nuxeo/integration-scripts-priv')
+            // Credentials
+            credentials('github-branch-source')
+          }
+          // Branches to build
+          branches('*/2021')
+          browser {
+            // Repository browser
+            githubWeb {
+              // URL
+              repoUrl('https://github.com/nuxeo/integration-scripts-priv')
+            }
+          }
+        }
+      }
+      // Lightweight checkout
+      lightweight(true)
+      // Script Path
+      scriptPath('Jenkinsfiles/publish-hf-release-notes.groovy')
+    }
+  }
+  parameters {
+    string {
+      name('HF_NUMBER')
+      defaultValue('')
+      description('A specific HF number (leave blank for latest HF).')
+      trim(true)
+    }
+  }
+}
