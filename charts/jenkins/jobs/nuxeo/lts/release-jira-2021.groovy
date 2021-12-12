79190c2512bbdd5daf02af7e7a1846a5c527fb93
Antoine Taillefer <ataillefer@nuxeo.com>
2021-12-12 22:25:10 +0100

    NXBT-3509: Add JIRA release job

---
+++ charts/jenkins/jobs/nuxeo/lts/release-jira-2021.groovy
@@ -0,0 +1,42 @@
+pipelineJob('nuxeo/lts/release-jira-2021') {
+  // Description
+  description('Release a given Nuxeo Server version in JIRA')
+  definition {
+    // Definition
+    cpsScm {
+      scm {
+        // SCM
+        git {
+          // Repositories
+          remote {
+            // Repository URL
+            url('https://github.com/nuxeo/integration-scripts-priv')
+            // Credentials
+            credentials('github-branch-source')
+          }
+          // Branches to build
+          branches('*/2021')
+          browser {
+            // Repository browser
+            githubWeb {
+              // URL
+              repoUrl('https://github.com/nuxeo/integration-scripts-priv')
+            }
+          }
+        }
+      }
+      // Lightweight checkout
+      lightweight(true)
+      // Script Path
+      scriptPath('Jenkinsfiles/release-jira.groovy')
+    }
+  }
+  parameters {
+    string {
+      name('NUXEO_BUILD_VERSION')
+      defaultValue('')
+      description('Version of the promoted Nuxeo Server build.')
+      trim(true)
+    }
+  }
+}
