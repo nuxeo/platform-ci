0391eb94212a8a87daff53accf0bb5977c20a4e4
Antoine Taillefer <ataillefer@nuxeo.com>
2022-01-27 15:12:04 +0100

    NXBT-3587: Remove usage of deprecated --export flag (Kubernetes >= 1.18)

--- secrets/import-secrets.sh
+++ secrets/import-secrets.sh
@@ -3,6 +3,7 @@ set -e
 
 for secret in $(cat $1)
 do
-  kubectl get secret $secret --namespace=$2 --export -o yaml |\
+  kubectl get secret $secret --namespace=$2 -o yaml | \
+    sed "s|namespace: $2|namespace: $NAMESPACE|g" | \
     kubectl apply --namespace=$NAMESPACE -f -
 done
