6690cfea718f96a25a77e6e21b7095ebbb3088ad
Kevin Leturc <kevin.leturc@hyland.com>
2022-08-17 18:10:35 +0200

    NXP-30995: Define nuxeo-benchmark job

---
+++ charts/jenkins/jobs/nuxeo/lts/nuxeo-benchmark.groovy
@@ -0,0 +1,58 @@
+pipelineJob('nuxeo/lts/nuxeo-benchmark') {
+  // Description
+  description('Nuxeo Benchmark')
+  definition {
+    // Definition
+    cpsScm {
+      scm {
+        // SCM
+        git {
+          // Repositories
+          remote {
+            // Repository URL
+            url('https://github.com/nuxeo/nuxeo-lts')
+            // Credentials
+            credentials('github-branch-source')
+          }
+          // Branches to build
+          branches('^${NUXEO_BRANCH}')
+          browser {
+            // Repository browser
+            githubWeb {
+              // URL
+              repoUrl('https://github.com/nuxeo/nuxeo-lts')
+            }
+          }
+        }
+      }
+      // no lightweight checkout in order to make branch parameter work
+      lightweight(false)
+      // Script Path
+      scriptPath('ci/Jenkinsfiles/benchmark.groovy')
+    }
+  }
+  parameters {
+    string {
+      name('NUXEO_BRANCH')
+      defaultValue('2021')
+      description('Branch to bench.')
+      trim(true)
+    }
+    string {
+      name('NUXEO_BUILD_VERSION')
+      defaultValue('2021.x')
+      description('Version of the Nuxeo Server to bench.')
+      trim(true)
+    }
+    booleanParam {
+      name('DEBUG')
+      defaultValue(false)
+      description('Whether or not to sleep before destroying the stack')
+    }
+  }
+  properties {
+    disableConcurrentBuilds {
+      abortPrevious(false)
+    }
+  }
+}
