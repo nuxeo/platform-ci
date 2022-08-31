c4ee8870d904b60d1d87bf643cffd640357c4505
Nour AL KOTOB <nour.alkotob@hyland.com>
2022-08-31 18:38:50 +0200

    NXBT-3654: Add 2023 release jobs

---
+++ charts/jenkins/jobs/nuxeo/lts/2023/release-nuxeo-2023.groovy
@@ -0,0 +1,42 @@
+pipelineJob('nuxeo/lts/2023/release-nuxeo-2023') {
+  // Description
+  description('Promote a given Nuxeo Server build version as a release')
+  definition {
+    // Definition
+    cpsScm {
+      scm {
+        // SCM
+        git {
+          // Repositories
+          remote {
+            // Repository URL
+            url('https://github.com/nuxeo/nuxeo-lts')
+            // Credentials
+            credentials('github-branch-source')
+          }
+          // Branches to build
+          branches('*/2023')
+          browser {
+            // Repository browser
+            githubWeb {
+              // URL
+              repoUrl('https://github.com/nuxeo/nuxeo-lts')
+            }
+          }
+        }
+      }
+      // Lightweight checkout
+      lightweight(true)
+      // Script Path
+      scriptPath('ci/Jenkinsfiles/release.groovy')
+    }
+  }
+  parameters {
+    string {
+      name('NUXEO_BUILD_VERSION')
+      defaultValue('')
+      description('Version of the Nuxeo Server build to promote.')
+      trim(true)
+    }
+  }
+}
